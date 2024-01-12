57364fd4ea4635277e8193a7d9f476dc58dd02bb
Abel Soler <102069966+AbelSolerDev@users.noreply.github.com>
2024-01-12 20:52:02 +0100

    get y post funcionan

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -1,10 +1,32 @@
+import {Project} from '../models/Project.js'
 
 
-
-export const getProjects = (req, res) => {
-  res.send('getting projects')
+export const getProjects = async (req, res) => {
+  try {
+    const projects = await Project.findAll()
+    console.log(projects)
+    res.json(projects)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ message: error.message })
+  }
 }
 
-export const createProject = (req, res) => {
-  res.send('creating project')
+export const createProject = async (req, res) => {
+  try {
+    const {name, priority, description} = req.body
+
+    const newProject = await Project.create({
+      name,
+      description,
+      priority
+    })
+
+    console.log(newProject)
+    res.json(newProject)
+
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ message: error.message })
+  }
 }
